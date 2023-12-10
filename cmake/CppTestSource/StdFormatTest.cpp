eea6b6c469f708a4440a21b0be72799cc53a8d0c
Dusk_NM02 <Dusk.11th@outlook.com>
2023-12-10 18:10:01 +0800

    build: 完善对std::fmt的测试，现在clang 17.0.6无法通过相应的测试

--- cmake/CppTestSource/StdFormatTest.cpp
+++ cmake/CppTestSource/StdFormatTest.cpp
@@ -1,7 +1,26 @@
 #include <format>
 #include <iostream>
 
+namespace Test
+{
+    struct CustomType
+    {
+        int value;
+    };
+} // namespace Test
+
+template <>
+struct std::formatter<::Test::CustomType, char> : public formatter<std::string, char>
+{
+    static auto format(const ::Test::CustomType &test_impl, format_context &ctx) ->
+        typename std::remove_reference_t<decltype(ctx)>::iterator
+    {
+        return std::format_to(ctx.out(), "{}", test_impl.value);
+    }
+};
+
 int main()
 {
-    std::cout << std::format("Hellow std format in C++ {}", 20) << std::endl;
+    std::cout << std::format("Hello std format in C++ {}", 20) << std::endl;
+    std::cout << std::format("Calling test formatter {}", Test::CustomType{11});
 }
