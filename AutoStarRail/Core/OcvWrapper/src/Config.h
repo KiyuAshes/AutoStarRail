f29ce39854a70ba77450eb9c2edb2701682d98a4
Dusk_NM02 <Dusk.11th@outlook.com>
2024-02-03 21:18:38 +0800

    chore: 拆分Impl为OpenCV的包装和Ort的包装

---
+++ AutoStarRail/Core/OcvWrapper/src/Config.h
@@ -0,0 +1,17 @@
+#ifndef ASR_CORE_OCVWRAPPER_CONFIG_H
+#define ASR_CORE_OCVWRAPPER_CONFIG_H
+
+#include <AutoStarRail/AsrConfig.h>
+
+#define ASR_CORE_OCVWRAPPER_NS_BEGIN                                           \
+    ASR_NS_BEGIN namespace Core                                                \
+    {                                                                          \
+        namespace OcvWrapper                                                   \
+        {
+
+#define ASR_CORE_OCVWRAPPER_NS_END                                             \
+    }                                                                          \
+    }                                                                          \
+    ASR_NS_END
+
+#endif // ASR_CORE_OCVWRAPPER_CONFIG_H
--- include/AutoStarRail/AsrConfig.h
+++ include/AutoStarRail/AsrConfig.h
@@ -52,15 +52,20 @@
 #define ASR_DISABLE_WARNING_BEGIN ASR_PRAGMA(warning(push))
 
 #define ASR_IGNORE_UNUSED_PARAMETER ASR_PRAGMA(warning(disable : 4100))
+#define ASR_IGNORE_OPENCV_WARNING ASR_PRAGMA(warning(disable : 4100 5054))
 
 #elif defined(__GNUC__)
 #define ASR_DISABLE_WARNING_BEGIN ASR_PRAGMA(GCC diagnostic push)
 
 #define ASR_IGNORE_UNUSED_PARAMETER                                            \
     ASR_PRAGMA(GCC diagnostic ignored "-Wunused-parameter")
+#define ASR_IGNORE_OPENCV_WARNING                                              \
+    ASR_PRAGMA(GCC diagnostic ignored "-Wdeprecated-enum-enum-conversion")
 
 #elif defined(__clang__)
 #define ASR_DISABLE_WARNING_BEGIN ASR_PRAGMA(clang diagnostic push)
+#define ASR_IGNORE_OPENCV_WARNING                                              \
+    ASR_PRAGMA(clang diagnostic ignored "-Wc11-extensions")
 
 #define ASR_IGNORE_UNUSED_PARAMETER                                            \
     ASR_PRAGMA(clang diagnostic ignored "-Wunused-parameter")
