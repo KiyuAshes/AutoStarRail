e7d7ff52b809eb7b76b3954573c49f3b2fdb950a
Dusk_NM02 <Dusk.11th@outlook.com>
2024-03-20 01:24:24 +0800

    feat: 支持外部实时读取log

---
+++ AutoStarRail/Core/Logger/src/IAsrLogRequesterImpl.cpp
@@ -0,0 +1,63 @@
+#include "IAsrLogRequesterImpl.h"
+#include <AutoStarRail/Core/Logger/Logger.h>
+#include <AutoStarRail/Utils/QueryInterface.hpp>
+
+IAsrLogRequesterImpl::IAsrLogRequesterImpl(uint32_t max_buffer_size)
+    : buffer_{max_buffer_size},
+      sp_sink_{std::make_shared<spdlog::sinks::callback_sink_mt>(
+          [this](const spdlog::details::log_msg& message)
+          {
+              const auto&     payload = message.payload;
+              std::lock_guard guard{mutex_};
+              buffer_.push_back({ASR_FULL_RANGE_OF(payload)});
+          })}
+{
+    ASR_CORE_LOG_INFO(
+        "Initialize IAsrLogRequesterImpl successfully! This = {}. max_buffer_size = {}.",
+        ASR::Utils::VoidP(this),
+        max_buffer_size);
+}
+
+AsrResult IAsrLogRequesterImpl::QueryInterface(
+    const AsrGuid& iid,
+    void**         pp_object)
+{
+    return ASR::Utils::QueryInterface<IAsrLogRequester>(this, iid, pp_object);
+}
+
+AsrResult IAsrLogRequesterImpl::RequestOne(IAsrLogReader* p_reader)
+{
+    std::lock_guard guard{mutex_};
+
+    const auto& message = buffer_.front();
+    const auto  result = p_reader->ReadOne(message.c_str());
+    buffer_.pop_front();
+
+    return result;
+}
+
+auto IAsrLogRequesterImpl::GetSink() const noexcept -> decltype(sp_sink_)
+{
+    return sp_sink_;
+}
+
+AsrResult CreateIAsrLogRequester(
+    uint32_t           max_line_count,
+    IAsrLogRequester** pp_out_requester)
+{
+    ASR_UTILS_CHECK_POINTER(pp_out_requester)
+
+    try
+    {
+        const auto p_result = new IAsrLogRequesterImpl{max_line_count};
+        auto&      sinks = ASR::Core::g_logger->sinks();
+        sinks.emplace_back(p_result->GetSink());
+        *pp_out_requester = p_result;
+        p_result->AddRef();
+        return ASR_S_OK;
+    }
+    catch (const std::bad_alloc&)
+    {
+        return ASR_E_OUT_OF_MEMORY;
+    }
+}
---
+++ AutoStarRail/Core/Logger/src/IAsrLogRequesterImpl.h
@@ -0,0 +1,26 @@
+#ifndef ASR_CORE_LOGGER_IASRLOGREQUESTERIMPL_H
+#define ASR_CORE_LOGGER_IASRLOGREQUESTERIMPL_H
+
+#include <AutoStarRail/ExportInterface/AsrLogger.h>
+#include <AutoStarRail/Utils/CommonUtils.hpp>
+#include <boost/circular_buffer/base.hpp>
+#include <spdlog/sinks/callback_sink.h>
+
+class IAsrLogRequesterImpl : public IAsrLogRequester
+{
+    using Type = std::string;
+    std::mutex                                         mutex_{};
+    boost::circular_buffer<Type, std::allocator<Type>> buffer_;
+    std::shared_ptr<spdlog::sinks::callback_sink_mt>   sp_sink_;
+    ASR_UTILS_IASRBASE_AUTO_IMPL(IAsrLogRequesterImpl)
+public:
+    IAsrLogRequesterImpl(uint32_t max_buffer_size);
+    // IAsrBase
+    AsrResult QueryInterface(const AsrGuid& iid, void** pp_object) override;
+    // IAsrLogRequester
+    AsrResult RequestOne(IAsrLogReader* p_reader) override;
+    // IAsrLogRequesterImpl
+    auto GetSink() const noexcept -> decltype(sp_sink_);
+};
+
+#endif // ASR_CORE_LOGGER_IASRLOGREQUESTERIMPL_H
