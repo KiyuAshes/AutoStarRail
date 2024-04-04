3701e0fd000fef2d48e9f67805b6e6e50bb3466d
Dusk_NM02 <Dusk.11th@outlook.com>
2024-04-04 16:09:10 +0800

    feature: 追加对boost::url的测试

--- AutoStarRail/AdditionalTest/BoostUrlTest.cpp
+++ AutoStarRail/AdditionalTest/BoostUrlTest.cpp
@@ -1,4 +1,3 @@
-#include "boost/url/urls.hpp"
 #include <boost/url.hpp>
 #include <gtest/gtest.h>
 #include <iostream>
@@ -24,4 +23,16 @@ TEST(BoostImplementationCheck, UrlPartlyResolvedTest)
     boost::url test{"adb://192.168.1.1:12345"};
     EXPECT_EQ(test.host_ipv4_address().to_string(), "192.168.1.1");
     EXPECT_EQ(test.port_number(), 12345);
+    EXPECT_EQ(test.authority().buffer(), std::string_view{"192.168.1.1:12345"});
+    EXPECT_EQ(test.scheme(), "adb");
+
+    boost::url test2{"hwnd://0x34143643"};
+    EXPECT_EQ(test2.host(), "0x34143643");
+    EXPECT_EQ(test2.authority().buffer(), std::string_view{"0x34143643"});
+    EXPECT_EQ(test2.scheme(), "hwnd");
+
+    boost::url test3{"hwnd://34143643"};
+    EXPECT_EQ(test3.host(), "34143643");
+    EXPECT_EQ(test3.authority().buffer(), std::string_view{"34143643"});
+    EXPECT_EQ(test3.scheme(), "hwnd");
 }
