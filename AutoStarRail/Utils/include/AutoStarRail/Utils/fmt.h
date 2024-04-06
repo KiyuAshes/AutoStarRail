52ea0427e843d0f64295db5dc2cfcb3674dcb4dc
Dusk_NM02 <Dusk.11th@outlook.com>
2024-04-07 01:06:47 +0800

    fix: 修复使用标准库fmt导致的编译失败问题

--- AutoStarRail/Utils/include/AutoStarRail/Utils/fmt.h
+++ AutoStarRail/Utils/include/AutoStarRail/Utils/fmt.h
@@ -4,24 +4,42 @@
 #include <AutoStarRail/AsrConfig.h>
 
 #ifndef ASR_USE_STD_FMT
-#define  ASR_USE_STD_FMT 1
+#define ASR_USE_STD_FMT 1
 #endif
 
 #if ASR_USE_STD_FMT
 #include <format>
 ASR_NS_BEGIN
-    namespace fmt {
-        using namespace ::std;
+namespace fmt
+{
+    using namespace ::std;
+}
+namespace FmtCommon
+{
+    template <class T>
+    auto to_string(T&& value)
+    {
+        return ASR::fmt::format("{}", std::forward<T>(value));
     }
+}
 ASR_NS_END
 #define ASR_FMT_NS std
 #else
 #include <fmt/core.h>
 #include <fmt/format.h>
 ASR_NS_BEGIN
-    namespace fmt {
-        using namespace ::fmt;
+namespace fmt
+{
+    using namespace ::fmt;
+}
+namespace FmtCommon
+{
+    template <class T>
+    auto to_string(T&& value)
+    {
+        return ASR::fmt::to_string(std::forward<T>(value));
     }
+}
 ASR_NS_END
 #define ASR_FMT_NS fmt
 #endif
